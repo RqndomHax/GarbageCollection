46f501ecb895aa7c2812334651ac9f8c9f260663
Paul COMTE <paul.codepro@gmail.com>
2021-10-26 19:23:00 +0200

    :tada: init garbage collection project

---
+++ includes/garbage_collector.h
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2021
+** garbage_collection
+** File description:
+** garbage collector structure
+*/
+
+#ifndef GARBAGE_COLLECTION_H_
+#define GARBAGE_COLLECTION_H_
+
+#endif /* !GARBAGE_COLLECTION_H_ */
---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2021
+** garbage collection
+** File description:
+** main
+*/
+
+#include "includes/garbage_collector.h"
+#include <stdio.h>
+
+int main(void)
+{
+    return (0);
+}
